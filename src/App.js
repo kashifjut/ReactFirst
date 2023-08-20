08709abed60f7634b115dde368a816a5a2d37f3d
Kashif riasat <kashifriasat11@gmail.com>
2023-08-20 18:09:26 +0500

    New File Added

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import Cards from "./components/Cards";
 import Navbar from "./components/Navbar";  
 import Prenav from "./components/Prenav";
 import Counter from "./components/Counter";
-
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Effect from "./components/Effect";
 
 function App() {
   return (
@@ -13,6 +13,7 @@ function App() {
     <Counter/>
     <Navbar/>
     <Cards/>
+    <Effect/>
     <ToastContainer 
     position="top-right"
     autoClose={5000}
---
+++ src/components/Effect.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Effect() {
+  return (
+    <div>
+      <h1>Effect</h1>
+    </div>
+  )
+}
+
+export default Effect
